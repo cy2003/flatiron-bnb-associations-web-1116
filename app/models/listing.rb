d235f14c7eafe0cb179e27dd96116cb408b75f28
cy2003 <catyyee@hotmail.com>
2017-01-03 21:38:17 -0500

    Done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,6 @@ class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
-  has_many :guests, :through => :reservations, :source => :reviews
+  has_many :guests, :through => :reservations, :foreign_key => :guest_id
+  has_many :reviews, :through => :guests, :foreign_key => :reservation_id
 end
